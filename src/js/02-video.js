d314789ea5edc93c683adf15bfffb311092be405
gulen13 <vitaliy.gulen@gmail.com>
2023-03-10 23:11:49 +0200

    task-2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+updateTime();
+
+player.on('timeupdate', _throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate (event) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, event.seconds);
+}
+
+function updateTime() {
+  const savedTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+}
